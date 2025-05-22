cf8fa937b2b43d85cb5e10fce39100fcf8bac9f7
Jonatan <jonatanav255@hotmail.com>
2025-05-21 23:40:43 -0600

    updating index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,33 @@
+// src/demo.ts
 import { EventLoop } from "./eventLoop";
 
-const example = new EventLoop()
+const loop = new EventLoop();
 
-console.log(example)
+// mimic setTimeout(fn, 0)
+function mySetTimeout(fn: () => void) {
+  loop.enqueueMacrotask(fn);
+}
+
+// mimic Promise.resolve().then(fn)
+function myThen(fn: () => void) {
+  loop.enqueueMicrotask(fn);
+}
+
+// Our “main script”
+console.log("start");
+
+mySetTimeout(() => {
+  console.log("in macrotask");
+  myThen(() => console.log("microtask inside macrotask"));
+});
+
+myThen(() => {
+  console.log("in microtask");
+  mySetTimeout(() => console.log("macrotask inside microtask"));
+});
+
+console.log("end");
+
+// Finally, kick the loop into gear:
+loop.run();
+console.log("all done");
